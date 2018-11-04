5fe325adc9d209885c31f2d37b6ba2195271b732
Thuy Dao Xuan <thuydx.9598@gmail.com>
2018-11-04 13:20:53 +0700

    Revert "Add new project"
    This reverts commit 04c6afefb74d9fcf2f16da17195bf5245e9fa7f5.

---
+++ Lightoxs/LightTox/Areas/User/Controllers/ThietBiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LightTox.Areas.User.Controllers
+{
+    public class ThietBiController : Controller
+    {
+        // GET: User/ThietBi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
