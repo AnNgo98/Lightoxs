66451977c875abe0bc9afb0fa3777bd38b0e0ffe
Thuy Dao Xuan <thuydx.9598@gmail.com>
2018-11-28 13:16:39 +0700

    commit admin post page

--- Lightoxs/LightTox/Areas/Admin/Controllers/HomeController.cs
+++ Lightoxs/LightTox/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace LightTox.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Admin/Home
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public string SaveNewPost(string contentPost)
+        {
+            string s = contentPost.ToString();
+            return s;
         }
     }
 }
