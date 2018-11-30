580c2d370ba8b3b652baab3707b7df7e85c8daff
Thuy Dao Xuan <thuydx.9598@gmail.com>
2018-11-30 10:43:32 +0700

    commit master

--- Lightoxs/LightTox/Areas/Admin/Controllers/HomeController.cs
+++ Lightoxs/LightTox/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using Lightoxs.Model;
 
 namespace LightTox.Areas.Admin.Controllers
 {
@@ -16,11 +18,18 @@ namespace LightTox.Areas.Admin.Controllers
             return View();
         }
 
-        [HttpPost]
-        public string SaveNewPost(string contentPost)
+        [HttpPost, ValidateInput(false)]
+        public ActionResult SaveNewPost(string title, string contentPost, string typeOfNews)
         {
-            string s = contentPost.ToString();
-            return s;
+            string path = Path.Combine(Server.MapPath(Constants.NEWS_CSSD_FILE_URL), CommonFunction.hashSHA256("XT~|KT") + ".txt");
+
+            using (var tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(contentPost);
+                tw.Close();
+            }
+
+            return View();
         }
     }
 }
---
+++ Lightoxs/LightTox/Model/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lightoxs.Model
+{
+    public static class Constants
+    {
+        public const string NEWS_CSSD_FILE_URL = "~/File/ChamSocSacDep/";
+    }
+}
